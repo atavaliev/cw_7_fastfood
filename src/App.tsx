eb431ce1c2c88d8ebf06117194305e3484a26a2c
Atavaliev Timur <tatavaliev@gmail.com>
2023-06-03 12:45:53 +0600

    Create main structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,28 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            
+        <div className="main-block">
+            <div className="order-block">
+                <div className="order-block--item">
+                    Order Item x 1 <span>delete</span>
+                </div>
+                <div>
+                    Total price: 0 GKS
+                </div>
+            </div>
+            <div className="menu-block">
+                <ul className="menu">
+                    <li className="menu-item">
+                        <div className="menu-item--img">
+                            <img src="" alt=""/>
+                        </div>
+                        <div>
+                            <div>Name</div>
+                            <div>Price KGS</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
